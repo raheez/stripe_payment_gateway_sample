171e38c6c59289b1a8bf96828aeea05a689ea8d3
raheez66 <raheez66@gmail.com>
2023-03-01 06:03:18 +0400

    added network calls for getting customer-id and ephermal keys

--- app/src/main/java/com/example/stripepaymentgateway/MainActivity.kt
+++ app/src/main/java/com/example/stripepaymentgateway/MainActivity.kt
@@ -1,31 +1,113 @@
 package com.example.stripepaymentgateway
 
+import android.app.VoiceInteractor.Request
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Toast
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.stripepaymentgateway.databinding.ActivityMainBinding
 import com.stripe.android.PaymentConfiguration
 import com.stripe.android.paymentsheet.PaymentSheet
+import com.stripe.android.paymentsheet.PaymentSheetResult
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var mainBinding: ActivityMainBinding
-    val PUBLISH_KEY = "pk_test_51Mfnd0JI0n14QA9nQB2R4pJWQ15zR7sL8oS7mny89AaENIUwGuq9MBz2kFnaoQ3JE6W7q9k4kd632GLZWCdPYcFn00DJwAjpmf"
-    val SECRET_KEY = "sk_test_51Mfnd0JI0n14QA9n8s6PboXTj53PjuDnk0UaDfjzE0SWA7Uc22OygRiGin7mpm0CkQqVCIPFakvtYiO44ZBVvom7008nddNBGn"
-    lateinit var paymentSheet : PaymentSheet
-    lateinit var paymentIntentClientSecret :String
-    lateinit var customerConfig :PaymentSheet.CustomerConfiguration
+    val PUBLISH_KEY =
+        "pk_test_51Mfnd0JI0n14QA9nQB2R4pJWQ15zR7sL8oS7mny89AaENIUwGuq9MBz2kFnaoQ3JE6W7q9k4kd632GLZWCdPYcFn00DJwAjpmf"
+    val SECRET_KEY =
+        "sk_test_51Mfnd0JI0n14QA9n8s6PboXTj53PjuDnk0UaDfjzE0SWA7Uc22OygRiGin7mpm0CkQqVCIPFakvtYiO44ZBVvom7008nddNBGn"
+    lateinit var paymentSheet: PaymentSheet
+    lateinit var paymentIntentClientSecret: String
+    lateinit var customerConfig: PaymentSheet.CustomerConfiguration
 
+    var mCustomerID = ""
+    var mEphemeralKey = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        paymentSheet = PaymentSheet(this, ::onPaymentSheetResult)
+
+        var mStrRequest = object : StringRequest(com.android.volley.Request.Method.POST,
+            "https://api.stripe.com/v1/customers", {
+
+                val mObject = JSONObject(it)
+                mCustomerID = mObject.getString("id")
+                Toast.makeText(this,"Customer-id obtained "+mCustomerID,Toast.LENGTH_SHORT).show()
+                getEphermalKey()
+
+            },
+            {
+
+            }
+
+        ) {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Authorization"] = "Bearer " + SECRET_KEY
+                return headers
+            }
+        };
+
+        val mRequestQueue = Volley.newRequestQueue(this)
+        mRequestQueue.add(mStrRequest)
+
         return setContentView(mainBinding.root)
 
-        PaymentConfiguration.init(this,PUBLISH_KEY)
-        paymentSheet.presentWithPaymentIntent(paymentIntentClientSecret,
-        PaymentSheet.Configuration(
-            merchantDisplayName = "Aafiyath",
-            customer =customerConfig))
+    }
+
+    private fun getEphermalKey() {
+        var mStrRequest = object : StringRequest(com.android.volley.Request.Method.POST,
+            "https://api.stripe.com/v1/ephemeral_keys", {
+
+                val mObject = JSONObject(it)
+                mEphemeralKey = mObject.getString("id")
+                Toast.makeText(this,"ephermal key obtained "+mEphemeralKey,Toast.LENGTH_SHORT).show()
+            },
+            {
+
+            }
+
+        ) {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Authorization"] = "Bearer " + SECRET_KEY
+                headers["Stripe-Version"] = "2022-11-15"
+                return headers
+            }
+
+            override fun getParams(): MutableMap<String, String>? {
+                val params = HashMap<String, String>()
+                params["customer"] = mCustomerID
+                return params
+            }
+        };
+
+        val mRequestQueue = Volley.newRequestQueue(this)
+        mRequestQueue.add(mStrRequest)
+    }
 
 
+
+    fun onPaymentSheetResult(paymentSheetResult: PaymentSheetResult) {
+        when (paymentSheetResult) {
+            is PaymentSheetResult.Canceled -> {
+                print("Canceled")
+            }
+            is PaymentSheetResult.Failed -> {
+                print("Error: ${paymentSheetResult.error}")
+            }
+            is PaymentSheetResult.Completed -> {
+                // Display for example, an order confirmation screen
+                print("Completed")
+            }
+        }
     }
 }
