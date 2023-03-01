17c5667f5b0de69ca388bb4a9425b15718514ae7
raheez66 <raheez66@gmail.com>
2023-03-01 17:15:29 +0400

    commit before pull

--- app/src/main/java/com/example/stripepaymentgateway/MainActivity.kt
+++ app/src/main/java/com/example/stripepaymentgateway/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.stripepaymentgateway
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.stripepaymentgateway.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding?.root)
+
+        mainBinding?.payButton?.setOnClickListener {
+            Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
